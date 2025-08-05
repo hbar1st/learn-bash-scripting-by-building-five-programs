8dfd5a87a069b0f0bc7c111490de6be78fa90287
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-05 13:43:14 -0400

    Save progress: 700.1

--- countdown.sh
+++ countdown.sh
@@ -3,11 +3,13 @@ echo -e "\n~~ Countdown Timer ~~\n"
 # Program that counts down to zero from a given argument
 if [[ $1 -gt 0 ]]
 then
+: '
   for (( i = $1; i >= 0; i-- ))
   do
     echo $i
     sleep 1
   done
+'
 else
   echo Include a positive integer as the first argument.
 fi
